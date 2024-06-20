84911faa09417a043719deff7a15ded64003e700
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-06-20 10:14:05 +0530

    add search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
 import ScrollToTop from './components/ScrollToTop'
+import Search from './pages/Search'
 
 const App = () => {
   return (
@@ -35,6 +36,7 @@ const App = () => {
         </Route>
         <Route path='/projects' element={<Projects />} />
         <Route path='/post/:postSlug' element={<PostPage />} />
+        <Route path='/search' element={<Search />} />
         
       </Routes>
       <Footer />
